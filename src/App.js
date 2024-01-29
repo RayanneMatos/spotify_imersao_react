70c04b60d528982b4952cf1d8d5a1fb914d8ec85
RayanneMatos <rayanne.contabeis@gmail.com>
2024-01-29 16:07:36 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Header from './Header/Header';
+import Sidebar from './Sidebar/Sidebar';
+import Footer from './Footer/Footer';
+import Playlist from './Playlist/Playlist';
+
+
+
+function App() {
+  return (
+    <> <Header></Header>
+      <Sidebar></Sidebar>
+      <Footer></Footer>
+      <Playlist></Playlist>
+    </>
+  );
+}
+
+export default App;
